e38772f03d8bbeb92449649e919c2e2e5613b2f0
Janis Streib <janis@dogcraft.de>
2016-06-14 14:04:40 +0200

    ADD: rudimentary questions overview

--- model.py
+++ model.py
@@ -1,9 +1,16 @@
+import sciencequiz
+
+
 class Question(object):
     def __init__(self, res):
         pass
 
 
 class Category(object):
-    def __init__(self, id, name):
+    def __init__(self, id, name, db):
         self.id = int(id)
         self.name = str(name)
+        self.db = db
+
+    def get_all_questions(self):
+        return self.db.execute("SELECT * FROM questions WHERE category = %s", (self.id,))
