1069043abef7ce82f7db669d28b6fcc8fca0bb3c
Janis Streib <janis@dogcraft.de>
2016-06-14 14:35:49 +0200

    CLN: removed debug output

--- model.py
+++ model.py
@@ -1,6 +1,3 @@
-import sciencequiz
-
-
 class Question(object):
     def __init__(self, id, question, category, image, db):
         self.id = id
@@ -16,14 +13,12 @@ class Category(object):
         self.db = db
 
     def get_all_questions(self):
-        print(self.db.execute("SELECT * FROM questions WHERE category = %s", (self.id,)))
         return [Question(**q, db=self.db) for q in
                 self.db.execute("SELECT * FROM questions WHERE category = %s", (self.id,))]
 
     @staticmethod
     def get_by_id(cat, db):
         res = db.execute("SELECT * FROM categories WHERE id = %s", (cat,))
-        print(res)
         return Category(**res[0], db=db)
 
 
